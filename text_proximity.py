aec546675f652dd5ea2db4c7f4c8b5a4d9cedf77
mancap314 <egregius314@gmail.com>
2018-10-24 01:13:01 +0200

    refactorisation + epsilon introduced

--- text_proximity.py
+++ text_proximity.py
@@ -1,4 +1,4 @@
-import os, re, time
+import os, re, time, itertools
 import numpy as np
 import pandas as pd
 from sklearn.metrics import average_precision_score
@@ -38,15 +38,14 @@ def get_articles_matrices(articles):
     # Get their vectorization
     words_vector = get_words_vector(word_list)
     duration = round(time.time() - t0, 1)
-    print(f'word_vector computed in {duration}s')
+    print(f'matrices for articles computed in {duration}s')
 
     for article in articles:
         article_words = re.findall(r'\w+', article.lower())
         article_matrix = None
         for i, word in enumerate(article_words):
             if word in words_vector:
-                # TODO: find better default distance
-                vector = np.append(words_vector[word], i / (len(article_words) - 1))
+                vector = words_vector[word]
                 vector = vector.reshape((vector.shape[0], 1))
                 if article_matrix is None:
                     article_matrix = vector
@@ -54,6 +53,15 @@ def get_articles_matrices(articles):
                     article_matrix = np.concatenate((article_matrix, vector), 1)
         result.append(article_matrix)
     return result
+
+
+def append_word_distance(article_matrix_list, eps):
+    for i, article_matrix in enumerate(article_matrix_list):
+        n_words = article_matrix.shape[1]
+        vec = np.array([eps * i / n_words for i in range(n_words)])
+        vec = vec.reshape((1, n_words))
+        article_matrix_list[i] = np.concatenate((article_matrix, vec), 0)
+    return article_matrix_list
 
 
 def get_sq_distances(matrix_0, matrix_1):
@@ -67,64 +75,64 @@ def get_sq_distances(matrix_0, matrix_1):
             sq_dists = sq_dist
         else:
             sq_dists = np.concatenate((sq_dists, sq_dist), 1)
-    return sq_dists
-
-
-def get_min_sq_distances(article_0, article_1):
-    matrices = get_articles_matrices([article_0, article_1])
-    sq_distances = get_sq_distances(matrices[0], matrices[1])
-    a = sq_distances.shape.index(min(sq_distances.shape))
-    return np.min(sq_distances, axis=a)
+    a = sq_dists.shape.index(min(sq_dists.shape))
+    return np.min(sq_dists, axis=a)
 
 
 def get_score(sq_distances, l=.1):
     return np.sum(np.exp(-sq_distances / l ** 2))
 
-# TODO: refactor calibrate and evaluate to externalize common actions
-# TODO: get matrices for all articles at once (much faster)
 
-def calibrate_l(df, col_0, col_1, col_score, l_values):
+def get_df_article_matrix_list(df, col_0, col_1):
+    articles_list = df[col_0].tolist() + df[col_1].tolist()
+    return get_articles_matrices(articles_list)
+
+
+def get_df_distances_list(article_matrix_list, eps):
+    n_pairs = int(len(article_matrix_list) / 2)
+    article_matrix_list = append_word_distance(article_matrix_list, eps)
     sq_distances_list = []
-    for i, row in df.iterrows():
-        sq_distances_list.append(get_min_sq_distances(row[col_0], row[col_1]))
+    for i in range(n_pairs):
+        sq_distances_list.append(get_sq_distances(article_matrix_list[i], article_matrix_list[n_pairs + i]))
+    return sq_distances_list
 
-    ap_score_best, l_best = -1, None
-    for l in l_values:
-        scores = [get_score(sq_distances, l) for sq_distances in sq_distances_list]
-        min_score, max_score = min(scores), max(scores)
-        scores = [(s - min_score) / (max_score - min_score) for s in scores]
-        scores = pd.Series(scores)
+
+def get_df_score(sq_distances_list, l):
+    scores = [get_score(sq_distances, l) for sq_distances in sq_distances_list]
+    min_score, max_score = min(scores), max(scores)
+    scores = [(s - min_score) / (max_score - min_score) for s in scores]
+    return pd.Series(scores)
+
+
+def calibrate_l(df, col_0, col_1, col_score, l_values, eps_values):
+    article_matrix_list = get_df_article_matrix_list(df, col_0, col_1)
+    ap_score_best, l_best, eps_best = -1, None, None
+    for l, eps in itertools.product(*[l_values, eps_values]):
+        sq_distances_list = get_df_distances_list(article_matrix_list, eps)
+        scores = get_df_score(sq_distances_list, l)
         ap_score = average_precision_score(df[col_score], scores)
         if ap_score > ap_score_best:
             ap_score_best = ap_score
             l_best = l
+            eps_best = eps
 
-    return l_best, ap_score_best
-
-
-def evaluate(df, col_0, col_1, l):
-    sq_distances_list = []
-    for i, row in df.iterrows():
-        sq_distances_list.append(get_min_sq_distances(row[col_0], row[col_1]))
-
-    scores = [get_score(sq_distances, l) for sq_distances in sq_distances_list]
-    min_score, max_score = min(scores), max(scores)
-    scores = [(s - min_score) / (max_score - min_score) for s in scores]
-    scores = pd.Series(scores)
-    return scores
+    return l_best, eps_best, ap_score_best
 
 
-# # Example score
-# score = compare('tomorrow it will rain. Horrible weather is expected', 'in one day there will be bad weather. People wait for better times')
-# print('score: {}'.format(score))
+def evaluate(df, col_0, col_1, l, eps):
+    article_matrix_list = get_df_article_matrix_list(df, col_0, col_1)
+    sq_distances_list = get_df_distances_list(article_matrix_list, eps)
+    return get_df_score(sq_distances_list, l)
+
+
 
 # # Example calibration
-# df = pd.read_csv(os.path.join('data', 'questions.csv')).iloc[:10]
-# l_best, ap_score_best = calibrate_l(df, 'question1', 'question2', 'is_duplicate', np.linspace(.001, 1, 100))
-# print('l_best: {}, corresponding best average precision: {}'.format(l_best, ap_score_best))
+# df = pd.read_csv(os.path.join('data', 'questions.csv')).iloc[:1000]
+# l_best, eps_best, ap_score_best = calibrate_l(df, 'question1', 'question2', 'is_duplicate', np.linspace(.0001, .2, 100), np.linspace(.001, 2, 100))
+# print('l_best: {}, eps_best: {}, corresponding best average precision: {}'.format(l_best, eps_best, ap_score_best))
 
-# Example evaluation
-df = pd.read_csv(os.path.join('data', 'questions.csv')).iloc[:10]
-scores = evaluate(df, 'question1', 'question2', l=.15)
-print(scores)
-print(df['is_duplicate'])
+# # Example evaluation
+# df = pd.read_csv(os.path.join('data', 'questions.csv')).iloc[:10]
+# scores = evaluate(df, 'question1', 'question2', l=.15)
+# print(scores)
+# print(df['is_duplicate'])

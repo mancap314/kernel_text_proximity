851a31eb91d23dc9613875165b69480b1d60087a
mancap314 <egregius314@gmail.com>
2018-10-24 21:14:04 +0200

    refactoring for more efficient pairwise computation

--- text_proximity.py
+++ text_proximity.py
@@ -13,14 +13,21 @@ def get_stop_words(language='english'):
     return stop_words
 
 
-def get_words_vector(words, word_embedding_file_path):
+def get_relevant_embeddings(articles, word_embedding_file_path, language='english', normalize=True):
     """
     builds a dictionary word -> vector for the words where a vector exisits
     :param words: list, words to look-up for
     :return: a 'word' -> vec (np.array) dictionary
     """
+    print('computing relevant word embeddings...')
+    t0 = time.time()
+    concatenation = ' '.join(*[articles]).lower()
+    words = re.findall(r'\w+', concatenation)
+    words = list(set(words))
+    stop_words = get_stop_words(language)
+    words = [word for word in words if word not in stop_words]
     words = [str(word).lower() for word in words]
-    res = {}
+    relevant_embeddings = {}
     with open(word_embedding_file_path) as word_embedding_file:
         # Go through the file lines until word found
         for line in word_embedding_file:
@@ -28,33 +35,24 @@ def get_words_vector(words, word_embedding_file_path):
             if current_word in words:
                 vec = line.split(' ')[1:]
                 vec = np.array([float(value) for value in vec])
-                res[current_word] = vec
+                if normalize:
+                    vec = vec / np.sum(vec)
+                relevant_embeddings[current_word] = vec
                 words.remove(current_word)
             if len(words) == 0:
                 break
+    print('relevant word embeddings computed in {}s'.format(round(time.time() - t0, 1)))
+    return relevant_embeddings
 
-    return res
 
-
-def get_articles_matrices(articles, word_embedding_file_path, language='english'):
+def get_articles_matrices(articles, relevant_embeddings):
     result = []
-    concatenation = ' '.join(*[articles]).lower()
-    word_list = re.findall(r'\w+', concatenation)
-    word_list = list(set(word_list))
-    stop_words = get_stop_words(language)
-    word_list = [word for word in word_list if word not in stop_words]
-    t0 = time.time()
-    # Get their vectorization
-    words_vector = get_words_vector(word_list, word_embedding_file_path)
-    duration = round(time.time() - t0, 1)
-    print(f'matrices for articles computed in {duration}s')
-
     for article in articles:
         article_words = re.findall(r'\w+', article.lower())
         article_matrix = None
         for i, word in enumerate(article_words):
-            if word in words_vector:
-                vector = words_vector[word]
+            if word in relevant_embeddings:
+                vector =relevant_embeddings[word]
                 vector = vector.reshape((vector.shape[0], 1))
                 if article_matrix is None:
                     article_matrix = vector
@@ -98,29 +96,27 @@ def get_score(sq_distances, l=.1):
     return np.mean(np.exp(-sq_distances / l ** 2))  # mean instead of sum
 
 
-def get_df_article_matrix_list(df, col_0, col_1, word_embedding_file_path, language='english'):
-    articles_list = df[col_0].tolist() + df[col_1].tolist()
-    return get_articles_matrices(articles_list, word_embedding_file_path, language)
+def get_scores_pairs(articles_0, articles_1, eps, l, relevant_embeddings):
+    scores = []
+    for i in range(len(articles_0)):
+        matrices = get_articles_matrices([articles_0[i], articles_1[i]], relevant_embeddings)
+        append_word_distance(matrices, eps)
+        matrix_0, matrix_1 = matrices[0], matrices[1]
+        sq_distances = get_sq_distances(matrix_0, matrix_1)
+        score = get_score(sq_distances, l)
+        scores.append(score)
+    min_score, max_score = min(scores), max(scores)
+    scores = pd.Series([(s - min_score) / (max_score - min_score) for s in scores])
+    return scores
 
 
-def get_df_distances_list(article_matrix_list, eps):
-    n_pairs = int(len(article_matrix_list) / 2)
-    article_matrix_list = append_word_distance(article_matrix_list, eps)
-    sq_distances_list = []
-    for i in range(n_pairs):
-        sq_distances_list.append(get_sq_distances(article_matrix_list[i], article_matrix_list[n_pairs + i]))
-    return sq_distances_list
+def calibrate(articles_0, articles_1, true_values, word_embedding_file_path, l_values, eps_values, prop=1, language='english'):
+    assert len(articles_0) == len(articles_1), 'article_0 (length={}) must have same length than article_1 (length={})'.format(len(articles_0), len(articles_1))
+    relevant_embeddings = get_relevant_embeddings(articles_0 + articles_1, word_embedding_file_path, language)
 
-
-def get_df_score(sq_distances_list, l):
-    scores = [get_score(sq_distances, l) for sq_distances in sq_distances_list]
-    min_score, max_score = min([s for s in scores if s > 0]), max([s for s in scores if s > 0])
-    scores = [(s - min_score) / (max_score - min_score) for s in scores]
-    return pd.Series(scores)
-
-
-def calibrate_l(df, col_0, col_1, col_score,  word_embedding_file_path, l_values, eps_values, prop=1, language='english'):
-    article_matrix_list = get_df_article_matrix_list(df, col_0, col_1,  word_embedding_file_path, language)
+    print('searching for best l and eps values...')
+    t0 = time.time()
+    y_true = pd.Series(true_values)
     ap_score_best, l_best, eps_best = -1, None, None
     values_to_test = list(itertools.product(*[l_values, eps_values]))
     indices_to_test = np.random.choice(len(values_to_test), size=int(round(prop * len(values_to_test))), replace=False)
@@ -128,37 +124,43 @@ def calibrate_l(df, col_0, col_1, col_score,  word_embedding_file_path, l_values
     values_to_test = list(operator.itemgetter(*indices_to_test)(values_to_test))
 
     for l, eps in values_to_test:
-        sq_distances_list = get_df_distances_list(article_matrix_list, eps)
-        scores = get_df_score(sq_distances_list, l)
-        ap_score = average_precision_score(df[col_score][scores >= 0], scores[scores >= 0])
+        scores = get_scores_pairs(articles_0, articles_1, eps, l, relevant_embeddings)
+        ap_score = average_precision_score(y_true[scores >= 0], scores[scores >= 0])
         if ap_score > ap_score_best:
             ap_score_best = ap_score
             l_best = l
             eps_best = eps
             print('better parameters found: l_best={}, eps_best={}, corresponding best average precision: {}'.format(l_best, eps_best, ap_score_best))
 
+    print('best l and eps values out of {} combinations from {} pairs computed in {}s'.format(len(values_to_test), len(articles_0), round(time.time() - t0, 1)))
     return l_best, eps_best, ap_score_best
 
 
-def evaluate(df, col_0, col_1, l, eps, word_embedding_file_path, language='english'):
-    article_matrix_list = get_df_article_matrix_list(df, col_0, col_1, word_embedding_file_path, language)
-    sq_distances_list = get_df_distances_list(article_matrix_list, eps)
-    return get_df_score(sq_distances_list, l)
+def evaluate(articles_0, articles_1, l, eps, word_embedding_file_path, language='english'):
+    assert len(articles_0) == len(articles_1), 'article_0 (length={}) must have same length than article_1 (length={})'.format(len(articles_0), len(articles_1))
+    relevant_embeddings = get_relevant_embeddings(articles_0 + articles_1, word_embedding_file_path, language)
+    print('computing pair scores...')
+    t0 = time.time()
+    scores = get_scores_pairs(articles_0, articles_1, eps, l, relevant_embeddings)
+    print('scores for {} pairs computed in {}s'.format(len(articles_0), round(time.time() - t0, 1)))
+    return scores
 
 
 # TODO: test handling of empty (None) article matrix
 
-# # Define embedding file
+# # # Define embedding file
 # NLP_RESOURCE_DIRECTORY = os.path.join(os.path.expanduser('~'), 'nlp-resources')
 # word_embedding_file_path = os.path.join(NLP_RESOURCE_DIRECTORY, 'glove.840B.300d.txt')
+
 # # Example calibration
-# df = pd.read_csv(os.path.join('data', 'questions.csv')).iloc[:1000]
-# l_best, eps_best, ap_score_best = calibrate_l(df, 'question1', 'question2', 'is_duplicate', word_embedding_file_path, np.linspace(.0001, .2, 100), np.linspace(.5, 1.5, 20), .1)
+# df = pd.read_csv(os.path.join('data', 'questions.csv')).iloc[:1000].dropna().reset_index(drop=True)
+# l_best, eps_best, ap_score_best = calibrate(df['question1'].tolist(), df['question2'].tolist(), df['is_duplicate'], word_embedding_file_path, np.linspace(.00001, .005, 100), np.linspace(.5, 3, 30), .1)
 # # l_best, eps_best, ap_score_best = calibrate_l(df, 'question1', 'question2', 'is_duplicate', np.linspace(.0001, .2, 100), [0], 1)
 # print('l_best: {}, eps_best: {}, corresponding best average precision: {}'.format(l_best, eps_best, ap_score_best))
+# # l_best: 0.0001, eps_best: 0.5526315789473684, corresponding best average precision: 0.5541396202974052
 
 # # Example evaluation
-# df = pd.read_csv(os.path.join('data', 'questions.csv')).iloc[:10]
-# scores = evaluate(df, 'question1', 'question2', l=.15)
-# print(scores)
-# print(df['is_duplicate'])
+# df = pd.read_csv(os.path.join('data', 'questions.csv')).dropna().reset_index(drop=True)
+# scores = evaluate(df['question1'].tolist(), df['question2'].tolist(), l=0.0001, eps=.55, word_embedding_file_path=word_embedding_file_path)
+# ap_score = average_precision_score(df['is_duplicate'][scores >= 0], scores[scores >= 0])
+# print('ap score: {}'.format(ap_score))
